2b8ce66b47f3f7cffe20306373d81e5a348bbbef
Heitor Murara <heitormurara@gmail.com>
2024-04-10 15:02:58 -0500

    Init project

---
+++ Meowfolio/MeowfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowfolioApp.swift
+//  Meowfolio
+//
+//  Created by Heitor Murara on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MeowfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
